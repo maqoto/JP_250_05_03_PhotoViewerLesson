365c1c724dbe8d02b0045afd470f3eca9cf3f41b
maqoto fooci <maqoto@gmail.com>
2016-11-28 21:16:55 +0900

    Initial Commit

---
+++ JP_251_07_04_PhotoViewerLesson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JP_251_07_04_PhotoViewerLesson
+//
+//  Created by maqoto fooci on 2016/11/28.
+//  Copyright © 2016年 maqoto fooci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
